eeceb6ab474ec1ecb1ad11186aeafe804fd58bb7
kittenrage <anacho-ipt23@lll.kpi.ua>
2021-09-11 04:33:19 +0300

    entropy

--- cp1/chorniy_fb-95_cp1/cp1.py
+++ cp1/chorniy_fb-95_cp1/cp1.py
@@ -1,25 +1,41 @@
 from collections import Counter
+import math
 
-al = {'й':0,'ц':0,'у':0,'к':0,'е':0,'н':0,'г':0,'ш':0,'щ':0,'з':0,'х':0,'ъ':0,'ф':0,'ы':0,'в':0,'а':0,'п':0,'р':0,'о':0,'л':0,'д':0,'ж':0,'э':0,'я':0,'ч':0,'с':0,'м':0,'и':0,'т':0,'ь':0,'б':0,'ю':0}
+al = {'й':0,'ц':0,'у':0,'к':0,'е':0,'н':0,'г':0,'ш':0,'щ':0,'з':0,'х':0,'ъ':0,'ф':0,'ы':0,'в':0,'а':0,'п':0,'р':0,'о':0,'л':0,'д':0,'ж':0,'э':0,'я':0,'ч':0,'с':0,'м':0,'и':0,'т':0,'ь':0,'б':0,'ю':0,' ':10}
 let = al.keys()
 f = open('text.txt',"r",encoding = "utf-8")
 text = f.read()
 text = text.lower()
+text1 = "".join([i for i in text if i in al.keys()])     #ridding off nonletters
+del al[" "]
+text2 = "".join([i for i in text if i in al.keys()])
 
-text = "".join([i for i in text if i in al.keys()])     #ridding off nonletters
-for i in text:
-    if (i in let):
-        al[i] = al[i] + 1                               #setting ammount of letters
+r = lambda x: 1 - (x / math.log2(32))
 
-bis = []
-for i,j in enumerate(text):
-    bi = text[i:i+2]                    #setting pairs
-    if (len(bi) == 2):
-        bis.append(bi)
+def letters(text, al):
+    for i in text:
+        if (i in let):
+            al[i] = al[i] + 1                               #setting ammount of letters
+    for i in al.keys():
+        al[i] /= len(text)
+    h = sum(list(map(lambda x: -x * math.log2(x), al.values())))    #entropy
+    print(h)
+    print(r(h))         #redundency 
 
-print(len(bis))                         #amount of all bigrams
-print(len(Counter(bis).keys()))         #amount of unique bigrams
+def bigrams(text):
+    bis = []
+    for i,j in enumerate(text):
+        bi = text[i:i+2]                    #setting pairs
+        if (len(bi) == 2):
+            bis.append(bi)
+    bisd =  Counter(bis)
+    for i in bisd.keys():
+        bisd[i] /= len(text)
+    h = sum(list(map(lambda x: -x * math.log2(x), bisd.values()))) / 2      #entropy
+    print(h)
+    print(r(h))         #redundency
 
-# print(sum(al.values()))
-# length = len(text)
-# print(length)
+letters(text1, al)
+bigrams(text1)
+letters(text2, al)
+bigrams(text2)
