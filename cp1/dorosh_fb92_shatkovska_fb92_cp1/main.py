d7f160739407da5dfec75da84704f8b2a799ec43
Anastasiia Dorosh <anastasiia.dorosh@jmind.systems>
2021-09-16 17:56:35 +0300

    Added function for detection frequency of each of the symbols

--- cp1/dorosh_fb92_shatkovska_fb92_cp1/main.py
+++ cp1/dorosh_fb92_shatkovska_fb92_cp1/main.py
@@ -1,7 +1,7 @@
 # This is the 1st lab on Cryptology yet in progress by Dorosh and Shatkovska FB-92
 
 # Cleaning example text to match the criteria before doing the task
-def cleanText(txt):
+def clean_text(txt):
     with open(txt, 'r', encoding='utf-8') as file:
         text = file.read().lower()
 
@@ -17,4 +17,23 @@ def cleanText(txt):
     with open('exmpl.txt', 'w', encoding='utf-8') as file:
         file.write(text)
 
-cleanText('exmpl_unformatted.txt')
+
+def count_mono(txt, alphabet):
+    # dictionary for output
+    output_clean = {}
+
+    with open(txt, 'r', encoding='utf-8') as file:
+        text = file.read().lower()
+
+    for a in alphabet:
+        results = text.count(a)
+        output_clean.update({a: results})
+
+    return output_clean
+
+
+clean_text('exmpl_unformatted.txt')
+alphabet_clean = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ы', 'ь', 'э', 'ю', 'я']
+print(count_mono('exmpl_unformatted.txt', alphabet_clean))
+
+#print(resilt)
