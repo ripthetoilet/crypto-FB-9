5bad9016c926c3e9af37fa7690e4cfb442ff7b25
Anastasiia Dorosh <anastasiia.dorosh@jmind.systems>
2021-09-16 18:38:05 +0300

    Added function for detection frequency of bigrams

--- cp1/dorosh_fb92_shatkovska_fb92_cp1/main.py
+++ cp1/dorosh_fb92_shatkovska_fb92_cp1/main.py
@@ -1,5 +1,7 @@
 # This is the 1st lab on Cryptology yet in progress by Dorosh and Shatkovska FB-92
 
+from collections import Counter
+
 # Cleaning example text to match the criteria before doing the task
 def clean_text(txt):
     with open(txt, 'r', encoding='utf-8') as file:
@@ -17,23 +19,38 @@ def clean_text(txt):
     with open('exmpl.txt', 'w', encoding='utf-8') as file:
         file.write(text)
 
-
+# counting monograms
 def count_mono(txt, alphabet):
     # dictionary for output
-    output_clean = {}
+    output = {}
 
+    # change later
     with open(txt, 'r', encoding='utf-8') as file:
         text = file.read().lower()
 
     for a in alphabet:
         results = text.count(a)
-        output_clean.update({a: results})
+        output.update({a: results})
 
-    return output_clean
+    return output
+
+
+def count_bi(txt, alphabet):
+    # change later
+    with open(txt, 'r', encoding='utf-8') as file:
+        text = file.read().lower()
+
+    res = Counter(text[idx: idx + 2] for idx in range(len(text) - 1))
+
+    return dict(res)
 
 
 clean_text('exmpl_unformatted.txt')
-alphabet_clean = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ы', 'ь', 'э', 'ю', 'я']
-print(count_mono('exmpl_unformatted.txt', alphabet_clean))
 
-#print(resilt)
+alphabet_clean = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ы', 'ь', 'э', 'ю', 'я']
+alphabet_with_spaces = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ы', 'ь', 'э', 'ю', 'я', ' ']
+
+# test and debug
+print(count_mono('exmpl.txt', alphabet_clean))
+print(count_mono('exmpl.txt', alphabet_with_spaces))
+print(count_bi('exmpl.txt', alphabet_with_spaces))
