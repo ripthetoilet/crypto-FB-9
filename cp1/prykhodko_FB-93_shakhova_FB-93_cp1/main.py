62397237e3177e67b5ea4f783fdeac03b31a0af0
AndriiPrykhodko <andriyprikhodko96@gmail.com>
2021-09-27 18:56:57 +0300

    Changed some mistakes with bigrams

--- cp1/prykhodko_FB-93_shakhova_FB-93_cp1/main.py
+++ cp1/prykhodko_FB-93_shakhova_FB-93_cp1/main.py
@@ -16,6 +16,12 @@ alphabetWithoutSpaces = ['а','б','в','г','д','е','ж','з','и','й','к',
             'л','м','н','о','п','р','с','т','у','ф','х','ц',
             'ч','ш','щ','ы','ь','э','ю','я']
 
+#розбиваю текст на різні біграми
+CrossBigramWithSpaces = [textWithSpaces[i:i+2] for i in range(len(textWithSpaces))] #перехресна з пробілами
+NoCrossBigramWithSpaces = [textWithSpaces[i:i+2] for i in range(0,len(textWithSpaces),2)] # не перехресна з пробілами
+CrossBigramWithoutSpaces = [textWithoutSpaces[i:i+2] for i in range(len(textWithoutSpaces))] #перехресна без пробілів
+NoCrossBigramWithoutSpaces = [textWithoutSpaces[i:i+2] for i in range(0,len(textWithoutSpaces),2)] # не перехресна без пробілів
+
 def letters(alphabet,text):
     len_text = len(text)
     #створили словник
@@ -42,10 +48,12 @@ def letters(alphabet,text):
         h = round(-dict.get(alphabet[i])*log(dict.get(alphabet[i]),2),3)
         h1 = h1+h
 
-    print("H1 =",round(h1,3))
+    print("H1 =",round(h1,4))
 
-def bigram(alphabet,text):
-    len_text = len(text)
+
+def bigram(alphabet,bigram_list):
+    lenght = len(bigram_list)
+
     # створили матрицю розміром алфавіту + 1
     n = len(alphabet)+1
     matrix = [0] * n
@@ -66,14 +74,14 @@ def bigram(alphabet,text):
     for i in range(1,len(matrix[0])):
         for j in range(1,len(matrix[0])):
             big = matrix[0][i]+matrix[j][0]
-            matrix[i][j]= ('{:.5f}'.format(text.count(big)/len_text))
+            matrix[i][j]= ('{:.4f}'.format(bigram_list.count(big)/lenght))
 
     #вивід матриці
     for i in range(len(matrix)):
-        for j in range(len(matrix)):
+        for j in range(23,len(matrix)):
             if(i==0):
-                print(matrix[i][j],"         ",end="")
-            else: print(matrix[i][j],"   ",end="")
+                print(matrix[i][j],"      ",end="")
+            else: print(matrix[i][j]," ",end="")
         print()
 
     #підрахунок Н2 безпосередньо за значенням
@@ -83,13 +91,12 @@ def bigram(alphabet,text):
             if(float(matrix[i][j])!=0):
                 h2=h2+(-float(matrix[i][j])*(log(float(matrix[i][j]),2)))
 
-    h2=round(h2/2,3)
+    h2=round(h2/2,4)
     print("H2=",h2)
 
-letters(alphabetWithSpaces,textWithSpaces)
-print()
-bigram(alphabetWithSpaces,textWithSpaces)
-print()
-letters(alphabetWithoutSpaces,textWithoutSpaces)
-print()
-bigram(alphabetWithoutSpaces,textWithoutSpaces)
+#letters(alphabetWithSpaces,textWithSpaces)
+#letters(alphabetWithoutSpaces,textWithoutSpaces)
+#bigram(alphabetWithSpaces,CrossBigramWithSpaces)
+#bigram(alphabetWithSpaces,NoCrossBigramWithSpaces)
+#bigram(alphabetWithoutSpaces,CrossBigramWithoutSpaces)
+bigram(alphabetWithoutSpaces,NoCrossBigramWithoutSpaces)
