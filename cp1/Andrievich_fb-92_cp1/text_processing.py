583426b457c47bc3f88f31e6c9c094b73be75c5b
Nikita Tishkov <nikitatishkov77@gmail.com>
2021-09-27 23:44:25 +0300

    Clear folder

--- cp1/Andrievich_fb-92_cp1/text_processing.py
+++
@@ -1,7 +0,0 @@
-def process_text(alphabet):
-    with open("text.txt", "r", encoding='1251') as file:
-        text = file.read().replace("\n", " ").replace("\r", "").lower().replace("ё", "е").replace("ъ", "ь")
-        for char in text[:]:
-            if char not in alphabet and char != " ":
-                text = text.replace(char, "")
-        return " ".join(text.split())
