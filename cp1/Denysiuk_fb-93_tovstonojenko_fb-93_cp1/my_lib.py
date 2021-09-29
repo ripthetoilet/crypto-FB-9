c70ee19572f07132a6ad085aab5973ae61191626
Артем <oster2279@gmail.com>
2021-09-29 04:03:07 +0300

    Created function for creating list of bigrams

--- cp1/Denysiuk_fb-93_tovstonojenko_fb-93_cp1/my_lib.py
+++ cp1/Denysiuk_fb-93_tovstonojenko_fb-93_cp1/my_lib.py
@@ -8,8 +8,16 @@ def filter_text(file_name: str, with_witesspace: bool) -> str:
     return filtered if not with_witesspace else re.sub("[^а-я]+", ' ', filtered)
 
 
-def make_dict_of_chars_entry(text: str) -> dict:
+def make_dict_of_frequency_of_chars(text: str) -> dict:
     stats = defaultdict(int)
     for char in text:
         stats[char] += 1
-    return dict(stats)
+    stats = dict(stats)
+    for k in stats:
+        stats[k] = stats[k] / len(text)
+    return stats
+
+def make_list_of_bigram(text: str, step: int) -> list:
+    if len(text) % 2 == 1:
+        text = text + 'a'
+    return [text[i] + text[i + 1] for i in range(0, len(text) - 1, step)]
