17049eb07a9cad896b995b9f8f6c9f55af0de3cf
Artem Belz <kyzacrab@gmail.com>
2021-10-02 21:46:12 +0300

    Initial commit

---
+++ cp1/volkov_fb-94_kalinichev_fb-94_cp1/src/main.rs
@@ -0,0 +1,32 @@
+use anyhow::{Context, Result};
+use regex::Regex;
+use std::fs;
+use std::path::{Path, PathBuf};
+
+fn filter_file<P: AsRef<Path>>(path: P) -> Result<String> {
+    let file_name = path
+        .as_ref()
+        .file_name()
+        .context("Failed to get file name from path")?
+        .to_str()
+        .context("Path include non UTF-8 symbols")?
+        .to_string();
+
+    let filtered_file = PathBuf::from(format!("filtered_{}", file_name));
+    if filtered_file.exists() {
+        println!("File already filtered!");
+        Ok(fs::read_to_string(filtered_file)?)
+    } else {
+        let lowercase_text = fs::read_to_string(path)?.to_lowercase();
+        let russian_letters = Regex::new("[^а-я-ё ]+")?;
+        let text = russian_letters.replace_all(&lowercase_text, " ");
+        let multiple_whitespaces = Regex::new(r"\s+")?;
+        let filtered_text = multiple_whitespaces.replace_all(text.as_ref(), " ");
+        fs::write(filtered_file, filtered_text.as_bytes())?;
+        Ok(filtered_text.to_string())
+    }
+}
+
+fn main() {
+    let text = filter_file("text.txt").unwrap();
+}
