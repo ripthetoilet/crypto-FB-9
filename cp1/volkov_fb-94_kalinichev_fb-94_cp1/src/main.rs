c057e804ea6f31ab7bb0ceb1633aaf38e84398a6
Artem Belz <kyzacrab@gmail.com>
2021-10-03 20:00:39 +0300

    feat: add info output

--- cp1/volkov_fb-94_kalinichev_fb-94_cp1/src/main.rs
+++ cp1/volkov_fb-94_kalinichev_fb-94_cp1/src/main.rs
@@ -1,8 +1,28 @@
+use std::fmt::Debug;
+
+use crate::calculation::{Frequency, EntropyExt, RedundancyExt};
+
 mod calculation;
 mod filter;
 
+fn print_info<T: Debug>(frequency: Frequency<T>, n_gram: f32, letters_num: f32) {
+    println!("Frequency \
+    {:?}", frequency);
+    let entropy = frequency.calc_entropy(n_gram);
+    println!("Entropy: {}", entropy);
+    println!("Redundancy: {}", entropy.calc_redundancy(letters_num));
+}
+
 fn main() {
-    let text = filter::filter_file("text.txt").unwrap();
-    dbg!(calculation::count_monogram_frequency(&text));
-    dbg!(calculation::count_bigram_frequency(&text));
+    let spaces_text = filter::filter_file("text.txt").unwrap();
+    let no_spaces_text: String = spaces_text.chars().filter(|c| !c.is_whitespace()).collect();
+
+    println!("Text monogram with spaces: ");
+    print_info(calculation::count_monogram_frequency(&spaces_text), 1f32, 34f32);
+    println!("Text bigram with spaces: ");
+    print_info(calculation::count_bigram_frequency(&spaces_text), 2f32, 34f32);
+    println!("Text monogram without spaces: ");
+    print_info(calculation::count_monogram_frequency(&no_spaces_text), 1f32, 33f32);
+    println!("Text bigram without spaces: ");
+    print_info(calculation::count_bigram_frequency(&no_spaces_text), 2f32, 33f32);
 }
