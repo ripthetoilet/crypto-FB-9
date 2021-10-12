eaa0811dbf5a05475b4d39bc60e61412b25752af
Mariia Shreider <shr.masha2001@gmail.com>
2021-10-13 02:44:18 +0300

    letters entropy

--- cp1/shreider_zhembrovska_fb93/main.py
+++ cp1/shreider_zhembrovska_fb93/main.py
@@ -1,3 +1,5 @@
+import math
+
 alph = ["а","б","в","г","д","е","ё","ж","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ",
         "ъ","ы","ь","э","ю","я", " "]
 
@@ -29,4 +31,15 @@ with open('text.txt', 'r', encoding = 'utf-8') as file:
                 space = True
 
 
+def letters_entropy(text, freq):
+    length = len(text)
+    for i in range(34):
+        count = 0
+        for j in range(length):
+            if text[j] == alph[i]:
+                count += 1
+        if count/length == 0: #for text without spaces
+            continue
+        freq.update({alph[i]: count/length})
+    return -1 * sum(freq[k] * math.log(freq[k], 2) for k in freq)
 
