f5b2cab77ed3497c4c5f9b216f3043345af122cd
Артем <oster2279@gmail.com>
2021-10-12 23:05:20 +0300

    Generation of list of IOC and key implementation added

--- cp2/Denysiuk_fb-93_tovstonojenko_fb-93_cp2/vigenere_lib.py
+++ cp2/Denysiuk_fb-93_tovstonojenko_fb-93_cp2/vigenere_lib.py
@@ -1,17 +1,20 @@
 import sys
+
 sys.path.insert(0, '../../cp1/Denysiuk_fb-93_tovstonojenko_fb-93_cp1')
 import re
 import my_lib
 import random
 
-# LETTERS = sorted(list('абвгдежзийклмнопрстуфхцчшщыьэюя'))
-LETTERS = sorted('qwertyuiopasdfghjklzxcvbnm')
+LETTERS = sorted(list('абвгдежзийклмнопрстуфхцчшщыьъэюя'))
+# LETTERS = sorted('qwertyuiopasdfghjklzxcvbnm')
 DICT_OF_INDEXES = {LETTERS[b]: b for b in range(len(LETTERS))}
+FREQUENCY_OF_CHARS_RUS_LIST = list('оеаинтсрвлкмдпуяыьгзбчйхжшющэфъ')
 
-def filter_text(file_name: str, with_whitespace: bool) -> str:
+
+def filter_text(file_name: str) -> str:
     with open(file_name, mode='rt', encoding='UTF-8') as f:
         filtered = f.read().lower().replace("ё", "е")
-    return re.sub("[^а-я]+", '', filtered) if not with_whitespace else re.sub("[^а-я]+", ' ', filtered)
+    return re.sub("[^а-я]+", '', filtered)
 
 
 def generate_key(length: int) -> str:
@@ -36,10 +39,11 @@ def decrypt(cyphered_text: str, key: str) -> str:
 
 def count_index_of_coincidence(text):
     index_of_coincidence = 0
-    for i in my_lib.make_dict_of_frequency_of_chars().values():
+    for i in my_lib.make_dict_of_frequency_of_chars(text).values():
         index_of_coincidence += i * (i - 1)
     n = len(text)
-    return index_of_coincidence/(n*(n-1))
+    return index_of_coincidence / (n * (n - 1))
+
 
 def divide_the_text_into_blocks(text: str, length_of_key: int) -> list[str]:
     blocks = []
@@ -58,3 +62,17 @@ def generate_list_of_indexes_of_coincidence_for_text_with_length_of_key(length_o
     for j in blocks:
         result_list.append(count_index_of_coincidence(j))
     return result_list
+
+
+def generate_supposable_key(cyphered_text: str, len_of_key: int) -> str:
+    key = ''
+    for i in divide_the_text_into_blocks(cyphered_text, len_of_key):
+        sorted_by_frequency_chars = [i[0] for i in sorted(my_lib.make_dict_of_frequency_of_chars(i).items(),
+                                                          key=lambda item: item[1], reverse=True)]
+        chars = ''
+        for m, n in zip(sorted_by_frequency_chars, FREQUENCY_OF_CHARS_RUS_LIST):
+            shift_length = (len(LETTERS) + LETTERS.index(m) - LETTERS.index(n)) % len(LETTERS)
+            chars += LETTERS[shift_length]
+        key += most_frequent_char(chars)
+    return key
+
