805392578c0b32ef4ea5a8eb019576dc1cd2797d
Mariia Shreider <shr.masha2001@gmail.com>
2021-10-13 01:28:28 +0300

    texts editing

--- cp1/shreider_zhembrovska_fb93/main.py
+++ cp1/shreider_zhembrovska_fb93/main.py
@@ -1,21 +1,32 @@
 alph = ["а","б","в","г","д","е","ё","ж","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ",
-        "ъ","ы","ь","э","ю","я"]
+        "ъ","ы","ь","э","ю","я", " "]
 
 #text editing
-text = []
+text_spaces = []
+text_nospaces = []
+
 with open('text.txt', 'r', encoding = 'utf-8') as file:
+    file = file.read()
+    file = file.lower()
+    length = len(file)
+
     space = False
-    for line in file:
-        line = line.lower()
-        length = len(line)
-        for i in range(length):
-            if line[i] in alph:
-                text.append(line[i])
-                space = False
-            else:
+    for i in range(length):
+        if file[i] in alph:
+            if file[i] == " ":
                 if space == True:
                     continue
                 else:
-                    text.append(" ")
+                    text_spaces.append(" ")
                     space = True
+            else:
+                text_spaces.append(file[i])
+                text_nospaces.append(file[i])
+                space = False
+        else:
+            if space == False:
+                text_spaces.append(" ")
+                space = True
+
+
 
