e867235638ee866496631e567d8d62cb1c1a107e
daniloflek <58514722+daniloflek@users.noreply.github.com>
2021-10-18 13:50:49 +0300

    first commit

--- cp1/flekevchuk_fb-93_cp1/main.py
+++ cp1/flekevchuk_fb-93_cp1/main.py
@@ -5,13 +5,13 @@ from os import write
 
 file = open("my.txt", encoding="utf8")
 
-#f2 = open('test_gap.txt', 'w' ,encoding="utf8")
-#alphabet = "_абвгдежзийклмнопрстуфхцчшщыьэюя"
-#my_str = file.read().replace("ъ","ь").replace("ё","е").lower()
+f2 = open('test_gap.txt', 'w' ,encoding="utf8")
+alphabet = "_абвгдежзийклмнопрстуфхцчшщыьэюя"
+my_str = file.read().replace("ъ","ь").replace("ё","е").lower()
 
-f2 = open('test.txt', 'w' ,encoding="utf8")
-alphabet = "абвгдежзийклмнопрстуфхцчшщыьэюя"
-my_str = file.read().replace("ъ","ь").replace("ё","е").replace("_", "").lower()
+#f2 = open('test.txt', 'w' ,encoding="utf8")
+#alphabet = "абвгдежзийклмнопрстуфхцчшщыьэюя"
+#my_str = file.read().replace("ъ","ь").replace("ё","е").replace("_", "").lower()
 
 
 def Entr(counter, l, n=1):
@@ -37,25 +37,22 @@ def printTable(counter, alphabet, name):
   f2.write("\n")
 
 
+l =len(my_str)
 
-l = len(my_str)
-if l % 2 :
-  my_str += 'а'
-l = l + (l % 2)
-
+k=7
 
 arr = []
 j = 0
-while j <= l-1:
-  arr.append(my_str[j] + my_str[j+1])
-  j += 2
+while j <= l-k:
+  arr.append(my_str[j:j+k])
+  j += k
 
 
 arr1 = []
-k = 0
-while k < (l - 1):
-  arr1.append(my_str[k] + my_str[k+1])
-  k += 1
+f = 0
+while f < (l - 1):
+  arr1.append(my_str[f:f+k])
+  f += 1
 
 
 letters = OrderedDict(Counter(my_str).most_common()) 
@@ -76,12 +73,12 @@ print()
 
 
 print("ПРОСТI БIГРАМИ")
-print("ЕНТРОПIЯ:", Entr(cK, sum(cK.values()), 2), 2)
+print("ЕНТРОПIЯ:", Entr(cK, sum(cK.values()), k))
 printTable(cK, alphabet, "ПРОСТI БIГРАМИ")
 print()
 
 print("ПЕРЕХРЕСТНI БIГРАМИ")
-print("ЕНТРОПIЯ:", Entr(cS, sum(cS.values()), 2), 2)
+print("ЕНТРОПIЯ:", Entr(cS, sum(cS.values()), k))
 printTable(cS, alphabet, "ПЕРЕХРЕСТНI БIГРАМИ")
 
 
