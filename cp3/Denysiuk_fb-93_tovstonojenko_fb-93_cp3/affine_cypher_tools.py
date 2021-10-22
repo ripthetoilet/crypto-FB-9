a2628873165c3e597f2ad4f7b67da794a1df560c
Артем <oster2279@gmail.com>
2021-10-23 02:58:26 +0300

    Created cypher tools file

---
+++ cp3/Denysiuk_fb-93_tovstonojenko_fb-93_cp3/affine_cypher_tools.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.insert(0, '../../cp1/Denysiuk_fb-93_tovstonojenko_fb-93_cp1')
+
+LETTERS = sorted(list('абвгдежзийклмнопрстуфхцчшщыьэюя'))
+
+
+def calculate_bigramm(b1: str, b2: str) -> int:
+    pass
+
+
