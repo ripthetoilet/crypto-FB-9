9711f80797536e2a5245bd0794afd95de29eb326
Cordies <mihakross@gmail.com>
2021-10-23 03:26:31 +0300

    Added encryption bigram function

--- cp3/Denysiuk_fb-93_tovstonojenko_fb-93_cp3/affine_cypher_tools.py
+++ cp3/Denysiuk_fb-93_tovstonojenko_fb-93_cp3/affine_cypher_tools.py
@@ -1,10 +1,11 @@
 import sys
+
 sys.path.insert(0, '../../cp1/Denysiuk_fb-93_tovstonojenko_fb-93_cp1')
 
 LETTERS = sorted(list('абвгдежзийклмнопрстуфхцчшщыьэюя'))
 
 
-def calculate_bigramm(b1: str, b2: str) -> int:
-    pass
-
-
+def encrypt_bigram(bigram: str, key: tuple) -> int:
+    l = len(LETTERS)
+    x = LETTERS.index(bigram[0]) * l + LETTERS.index(bigram[1])
+    y = (key[0] * x + key[1]) % l ** 2
