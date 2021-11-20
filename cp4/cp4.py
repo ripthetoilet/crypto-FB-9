9bf1da9aa3f83aae873f41c8d40ebf14668da1a9
kittenrage <anacho-ipt23@lll.kpi.ua>
2021-11-20 11:57:56 +0200

    parts 3 + 4

--- cp4/cp4.py
+++ cp4/cp4.py
@@ -1,6 +1,15 @@
 import random 
 LOW = pow(2, 255) + 1
 HIGH = pow(2, 256) - 1
+msg = 'hello, my name is tolik its pleasure for me to meet you'
+
+gcd = lambda a, b: a if b == 0 else not a % b and b or gcd(b , a % b)
+encode = lambda msg: int(msg.encode('utf-8').hex(), 16)
+decode = lambda msg: bytes.fromhex(hex(msg)[2:]).decode('ASCII')
+encrypt = lambda msg, e, n: pow(encode(msg), e, n)
+decrypt = lambda msg, d, n: decode(pow(msg, d, n))
+sign = lambda msg, d, n: pow(encode(msg), d, n)
+verify = lambda msg, sign, e, n: True if encode(msg) == pow(sign, e, n) else False
 
 def milrab(num, r = 100):
     m = num - 1
@@ -25,8 +34,34 @@ def getran():
         if not milrab(i): continue
         else: return i
 
-pair1 = (getran(), getran())
-pair2 = (getran(), getran())
-if pair1[0] * pair1[1] > pair2[0] * pair2[1]: pair1, pair2 = pair2, pair1
-print(pair1)
-print(pair2)
+def genkey():
+    pair = (getran(), getran())
+    n = pair[0] * pair[1]
+    phi = (pair[0] - 1) * (pair[1] - 1)
+    e = random.randrange(2, phi)
+    while(gcd(e,phi) != 1): e = random.randrange(2, phi)
+    d = pow(e, -1, phi)
+    openkey = (n, e)
+    return (openkey, d)
+
+keysA  = genkey()
+NA, EA = keysA[0]
+da = keysA[1]
+print('na = ', NA)
+print('ea = ', EA)
+print('da = ', da)
+keysB  = genkey()
+NB, EB = keysB[0]
+db = keysB[1]
+print('\nnb = ', NB)
+print('eb = ', EB)
+print('db = ', db)
+while NA < NB: 
+    print('generate new keys for A')
+    keysA  = genkey()
+    NA, EA = keysA[0]
+    da = keysA[1]
+    print('na = ', NA)
+    print('ea = ', EA)
+    print('da = ', da)
+print('----------------------------------------------------')
