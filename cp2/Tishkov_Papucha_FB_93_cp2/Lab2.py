e62181e898c1743d6e6f2414451cac2c79103cfd
Nikita Tishkov <nikitatishkov77@gmail.com>
2021-11-22 18:17:17 +0200

    Add index calculation functionality

--- cp2/Tishkov_Papucha_FB_93_cp2/Lab2.py
+++ cp2/Tishkov_Papucha_FB_93_cp2/Lab2.py
@@ -1,5 +1,6 @@
 #coding=UTF-8
 import random
+import pandas as pd
 
 rus_alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к',
                 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х',
@@ -11,6 +12,33 @@ def calc_symbol_freq(symbol, text):
         if symbol == text_symbol:
             symbol_counter = symbol_counter + 1
     return symbol_counter / len(text)
+
+def calc_all_symbols_freq(alphabet, text, is_space_allowed = False, log_file_name = None):
+    symbol_num_dict = {}
+    symbol_freq_dict = {}
+    if is_space_allowed:
+        alphabet.append(' ')
+    for letter in alphabet:
+        symbol_num_dict.update({letter: 0})
+        symbol_freq_dict.update({letter: 0})
+    for symbol in text:
+        symbol_num_dict[symbol] = symbol_num_dict[symbol] + 1
+    for symbol in alphabet:
+        symbol_freq_dict[symbol] = symbol_num_dict[symbol] / len(text)
+    
+    if log_file_name != None:
+        sorted_symbol_freq_dict = dict(sorted(symbol_freq_dict.items(), key=lambda item: item[1], reverse=True))
+        out_df = pd.DataFrame.from_dict(sorted_symbol_freq_dict, orient='index', columns=['Frequency'])
+        out_df.to_excel(log_file_name + '.xlsx')
+
+    return symbol_freq_dict
+
+def generate_key(size):
+    key = ''
+    for i in range(0, size):
+        #TODO: It is better to find another solution
+        key += ''.join(random.choice(rus_alphabet))
+    return key
 
 def encrypt_letter(ch, key_letter, alphabet=rus_alphabet):
     ch_id = alphabet.index(ch)
@@ -38,23 +66,41 @@ def decrypt(enc_text, key, alphabet=rus_alphabet):
         dec_text.append(decrypt_letter(j, key[i % len(key)], alphabet))
     return "".join(dec_text) 
 
-def generate_key(size):
-    key = ''
-    for i in range(0, size):
-        #TODO: It is better to find another solution
-        key += ''.join(random.choice(rus_alphabet))
-    return key
+def count_considence_index(text):
+    index = 0
+    for i in calc_all_symbols_freq(rus_alphabet, text).values():
+        index += i * (i - 1)
+    n = len(text)
+    index /= n * (n - 1)
+    return index
 
 def main():
-    text = str()
-    with open('dyuma_cut.txt', 'r') as dyuma_file:
-        text = dyuma_file.read()
-    key = generate_key(2)
-    with open('enc_text1.txt', 'w') as enc_file1:
-        enc_file1.write(encrypt(text, key))
-    key = generate_key(10)
-    with open('enc_text2.txt', 'w') as enc_file2:
-        enc_file2.write(encrypt(text, key))
+    while(True):
+        print("To encrypt text: 2")
+        print("To calculate index: 1")
+        print("Quit: 0")
+        command = input()
+        if command == '0':
+            break
+        elif command == '1':
+            print("Type file name:")
+            file_name = input()
+            with open(file_name, 'r') as file:
+                text = file.read()
+                print(count_considence_index(text))
+        elif command == '2':
+            print("Type file name of original text:")
+            input_file_name = input()
+            print("Type size of key for encryption:")
+            key_size = int(input())
+            print("Enter file name for encrypted text:")
+            enc_file_name = input()
+            with open(input_file_name, 'r') as input_file:
+                input_text = input_file.read()
+                with open(enc_file_name, 'w') as output_file:
+                    key = generate_key(key_size)
+                    output_file.write(encrypt(input_text, key))
 
+                
 if __name__ == '__main__':
     main()
