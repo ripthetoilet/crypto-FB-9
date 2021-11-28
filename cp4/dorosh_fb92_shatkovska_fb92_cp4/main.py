8ffacc4052157759992457e8c9c78bef7668102f
diShat <diana.shatkovska@gmail.com>
2021-11-28 23:20:10 +0200

    miller-rabin test. needs review

--- cp4/dorosh_fb92_shatkovska_fb92_cp4/main.py
+++ cp4/dorosh_fb92_shatkovska_fb92_cp4/main.py
@@ -1,6 +1,6 @@
 # # This is the 4th lab on Cryptology yet in progress by Dorosh and Shatkovska FB-92
 import random
-
+rand = random.SystemRandom()
 
 # from lab3
 def gcd(a, b):
@@ -14,13 +14,34 @@ def gcd(a, b):
         p.append(p[-1]*(-q)+p[-2])
     return gcd_val, p[-2]
 
+
 # http://rosettacode.org/wiki/Miller%E2%80%93Rabin_primality_test#Python
 # Miller-Rabin primality test
-def miller(p: int):
+def miller_rabin(p, k):
+    # part 0
+    d = (p - 1) % 2
+    s = (p - 1) // 2
     counter = 0
-    while counter < p:
-        pass
-    x = random.randint(1, p)
-    if gcd(x, p) == 1:
+    while counter < k:
+        # part 1
+        x = rand.randint(1, p)
+        if gcd(x, p)[0] > 1:
+            return False
+        elif gcd(x, p)[0] == 1:
+            # part 2
+            if abs(pow(x, d, p)) == 1:
+                continue
+            else:
+                xr = pow(x, 2*d, p)         # if r == 1
+                for r in range(2, s - 1):
+                    xr = pow(xr, d * (2 ** r), p)
+                    if xr == -1:
+                        continue
+                    elif xr == -1:
+                        return False
+        counter += 1
+    return True
 
-    pass
+
+print(miller_rabin(97, 10))         # needs to return True
+
