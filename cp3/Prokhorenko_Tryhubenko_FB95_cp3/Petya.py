112d7c4e02da9325517c6c15bf8eae11414d1616
yp-afr <prohorenko.yaroslav@icloud.com>
2021-12-06 04:01:29 +0200

    smth was uploaded

--- cp3/Prokhorenko_Tryhubenko_FB95_cp3/Petya.py
+++ cp3/Prokhorenko_Tryhubenko_FB95_cp3/Petya.py
@@ -2,11 +2,45 @@ from cp3.Prokhorenko_Tryhubenko_FB95_cp3.PetyaUtils import PetyaUtils
 
 
 class Petya:
-    @staticmethod
-    def huef():
-        x = 2
-        y = 5
-        print(PetyaUtils.calculate_gcd(x, y))
+    def __init__(self):
+        self.most_frequent_bigrams = ['ст', 'но', 'то', 'на', 'ен']
+        self.alphabet = "абвгдежзийклмнопрстуфхцчшщыьэюя"
+
+    def crack(self):
+
+        possibles_keys: list = []
+        frequent_bigrams_cipher_text: dict = {}
+        for iterator_i in range(0, len(self.most_frequent_bigrams) - 1):
+            frequent_bigrams_cipher_text = PetyaUtils.get_frequent_bigrams("01.txt")
+            for iterator_j in range(0, len(frequent_bigrams_cipher_text) - 1):
+                difX = (self.alphabet.index(self.most_frequent_bigrams[iterator_i][0]) * len(
+                    self.alphabet) + self.alphabet.index(self.most_frequent_bigrams[iterator_i][1])) - (
+                               self.alphabet.index(self.most_frequent_bigrams[iterator_i + 1][0]) * len(
+                           self.alphabet) + self.alphabet.index(self.most_frequent_bigrams[iterator_i + 1][1]))
+                difY = (self.alphabet.index(frequent_bigrams_cipher_text[iterator_j][0]) * len(
+                    self.alphabet) + self.alphabet.index(frequent_bigrams_cipher_text[iterator_j][1])) - (
+                               self.alphabet.index(frequent_bigrams_cipher_text[iterator_j + 1][0]) * len(
+                           self.alphabet) + self.alphabet.index(frequent_bigrams_cipher_text[iterator_j + 1][1]))
+                possible_values = PetyaUtils.calculate_linear_comparisons(difX, difY, len(self.alphabet) ** 2)
+                if (type(possible_values) == list):
+                    for possible_value in possible_values:
+                        if PetyaUtils.calculate_gcd(possible_value, len(self.alphabet))[0] == 1:
+                            y = (self.alphabet.index(frequent_bigrams_cipher_text[iterator_j][0]) * len(
+                                self.alphabet) + self.alphabet.index(frequent_bigrams_cipher_text[iterator_j][1]))
+                            x = (self.alphabet.index(self.most_frequent_bigrams[iterator_i][0]) * len(
+                                self.alphabet) + self.alphabet.index(self.most_frequent_bigrams[iterator_i][1]))
+                            possibles_keys.append((possible_value, (y - x * possible_value) % len(self.alphabet) ** 2))
+        print(possibles_keys)
+        for possibles_key in possibles_keys:
+            a, b = possibles_key
+            plaintext = ""
+            for char in PetyaUtils.get_text("01.txt"):
+                plaintext += self.alphabet[(PetyaUtils.calculate_inverted(a, len(self.alphabet)) * (
+                            self.alphabet.index(char) + len(self.alphabet) - b)) % len(self.alphabet)]
+            print(plaintext + "\n\n\n")
+
+    # a,b -> foo1() -> text -> print(text) -> (approve || decline) by RUSSIAN rozpiznavach
 
 
-Petya.huef()
+p = Petya()
+p.crack()
--- cp3/Prokhorenko_Tryhubenko_FB95_cp3/PetyaUtils.py
+++ cp3/Prokhorenko_Tryhubenko_FB95_cp3/PetyaUtils.py
@@ -1,4 +1,12 @@
+import re
+from collections import Counter
+
+
 class PetyaUtils:
+    @staticmethod
+    def get_text(path_to_file):
+        return re.sub(r"[^а-яА-Я]", "", open(path_to_file).read()).lower()
+
     @staticmethod
     def calculate_gcd(first_number, second_number):
         if first_number == 0:
@@ -7,3 +15,23 @@ class PetyaUtils:
             gcd, x, y = PetyaUtils.calculate_gcd(second_number % first_number, first_number)
             return gcd, y - (second_number // first_number) * x, x
 
+    @staticmethod
+    def calculate_inverted(number, module):
+        gcd, x, y = PetyaUtils.calculate_gcd(number, module)
+        return x % module if gcd == 1 else None
+
+    @staticmethod
+    def get_frequent_bigrams(path_to_file):
+        text = PetyaUtils.get_text(path_to_file)
+        frequency = Counter(text[bi: bi + 2] for bi in range(len(text) - 1))
+        frequency = dict(sorted(frequency.items(), key=lambda x: x[1], reverse=True))
+        return list(dict(list(frequency.items())[:5]).keys())
+
+    @staticmethod
+    def calculate_linear_comparisons(difX, difY, module) -> list:
+        gcd, x, y = PetyaUtils.calculate_gcd(difX, module)
+        if gcd == 1:
+            return [(PetyaUtils.calculate_inverted(difX, module) * difY) % module]
+        elif difY % gcd == 0:
+            temp = ((difY / gcd) * PetyaUtils.calculate_inverted(difX / gcd, module / gcd)) % (module / gcd)
+            return [(temp + d * (module / gcd)) for d in range(0, gcd)]
