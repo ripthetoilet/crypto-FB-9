95c7ce02cc3af91c953c231402613922dc175623
Mariia Shreider <shr.masha2001@gmail.com>
2021-12-18 02:13:31 +0200

    optimized

--- cp4/shreider_zhembrovska_fb93_cp4/main.py
+++ cp4/shreider_zhembrovska_fb93_cp4/main.py
@@ -1,47 +1,5 @@
 import random
 
-def find_simple_num(n0, n1):
-    x = random.randint(n0, n1)
-    if x%2 == 0: m0 = x+1
-    else: m0 = x
-    for i in range(0, int((n1-m0)/2)+1):
-        if(is_simple(m0+2*i)) == 1: return m0+2*i
-    return find_simple_num(n0, n1)
-
-def is_simple(p):
-    if(p%3 == 0 or p%5 == 0 or p%7 == 0): return 0
-    else:
-        d = 1
-        while (True):
-            s = is_step2((p - 1) // d, 0)
-            if ((p - 1) % d == 0 and s != -1): break
-            d += 2
-        return miller_rabin(p,d,s,0)
-
-def miller_rabin(p,d,s,counter):
-    simp = 0
-    x = random.randint(1,p)
-    if gcd(x,p) != 1: return 0
-    if x**d%p == 1 or (x**d%p)-p == -1: simp = 1
-    else:
-        for r in range(1, s):
-            xr = (x**(d*2**r))%p
-            if xr - p == -1:
-                simp = 1
-                break
-            elif xr == 1: return 0
-    if simp == 0: return 0
-    counter += 1
-    if(counter < 10):
-        return miller_rabin(p,d,s,counter)
-    return 1
-
-def is_step2(n, s):
-    if(n == 1): return s
-    if(n == 2): return s+1
-    elif(n%2 == 0): return is_step2(n/2, s+1)
-    return -1
-
 def gcd(a, b):
     while a != 0 and b != 0:
         if a > b: a = a % b
@@ -58,16 +16,62 @@ def inverted(a, n):
     for i in range(2, len(q)): q[i] = q[i - 2] - q[i] * q[i - 1]
     return q[-2]
 
+def gorner(x,e,m):
+    e = bin(e)
+    y = 1
+    for i in e[2:]:
+        y = (y**2)%m
+        if int(i) == 1:
+            y = (y*x)%m
+    return y
+
+def find_simple(interval):
+    x = random.randint(interval[0], interval[1])
+    m0 = x if x%2 != 0 else x+1
+    for i in range(0, int((interval[1]-m0)/2)+1, 2):
+        if(is_simple(m0+i)) == 1: return m0+i
+    return find_simple(interval)
+
+def is_simple(p):
+    if(p%3 == 0 or p%5 == 0 or p%7 == 0): return 0
+    else: return miller_rabin(p)
+
+def ds(p):
+    d = p-1
+    s = 0
+    while(d%2 == 0):
+        d = d//2
+        s +=1
+    return d,s
+
+def miller_rabin(p):
+    d,s = ds(p)
+    for i in range(10):
+        simp = 0
+        x = random.randint(1,p)
+        if gcd(x,p) != 1: return 0
+        num = gorner(x,d,p)
+        if num == 1 or num-p == -1: simp = 1
+        else:
+            for r in range(1, s):
+                num = gorner(num,2,p)
+                if num - p == -1:
+                    simp = 1
+                    break
+                elif num == 1: return 0
+        if not simp: return 0
+    return 1
+
 def generate_pairs():
-    interval = [265,1000]
+    interval = [2**255,2**256]
     pq, pq1 = [], []
     for i in range(2):
-        pq.append(find_simple_num(interval[0],interval[1]))
-        pq1.append(find_simple_num(interval[0],interval[1]))
+        pq.append(find_simple(interval))
+        pq1.append(find_simple(interval))
     if (pq[0]*pq[1] > pq1[0]*pq1[1]): return generate_pairs()
     return [pq, pq1]
 
-def generate_keys():
+def GenerateKeyPair():
     pq = generate_pairs()
     n,fi,e,d = [],[],[],[]
     for i in range(2):
@@ -76,10 +80,14 @@ def generate_keys():
         e.append(random.randint(2, fi[i]-1))
         while (gcd(e[i], fi[i]) != 1):
             e[i] = random.randint(2, fi[i]-1)
-        d.append(inverted(e[i],fi[i]))
+        d.append((inverted(e[i],fi[i]))%fi[i])
+    open_keys = [(e[0],n[0]),(e[1],n[1])]
+    secret_key = d
+    return open_keys, secret_key
+
+def Encrypt():
     return 0
 
 
-print(generate_keys())
-
+print(GenerateKeyPair())
 
