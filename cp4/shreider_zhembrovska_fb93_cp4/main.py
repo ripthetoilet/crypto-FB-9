ad702df4eea5f20232ef1f34e03d477f28dfb3a8
Mariia Shreider <shr.masha2001@gmail.com>
2021-12-18 02:49:44 +0200

    generate keys

--- cp4/shreider_zhembrovska_fb93_cp4/main.py
+++ cp4/shreider_zhembrovska_fb93_cp4/main.py
@@ -1,4 +1,5 @@
 import random
+interval = [2**256, 2**257-2]
 
 def gcd(a, b):
     while a != 0 and b != 0:
@@ -6,7 +7,7 @@ def gcd(a, b):
         else: b = b % a
     return a + b
 
-def inverted(a, n):
+def inverse(a, n):
     q = [0, 1]
     while a != 0 and n != 0:
         if a > n:
@@ -25,14 +26,14 @@ def gorner(x,e,m):
             y = (y*x)%m
     return y
 
-def find_simple(interval):
+def get_prime():
     x = random.randint(interval[0], interval[1])
     m0 = x if x%2 != 0 else x+1
-    for i in range(0, int((interval[1]-m0)/2)+1, 2):
-        if(is_simple(m0+i)) == 1: return m0+i
-    return find_simple(interval)
+    for i in range(m0, interval[1], 2):
+        if(is_prime(i)) == 1: return i
+    return get_prime()
 
-def is_simple(p):
+def is_prime(p):
     if(p%3 == 0 or p%5 == 0 or p%7 == 0): return 0
     else: return miller_rabin(p)
 
@@ -47,43 +48,31 @@ def ds(p):
 def miller_rabin(p):
     d,s = ds(p)
     for i in range(10):
-        simp = 0
+        prime = 0
         x = random.randint(1,p)
         if gcd(x,p) != 1: return 0
         num = gorner(x,d,p)
-        if num == 1 or num-p == -1: simp = 1
+        if num == 1 or num-p == -1: prime = 1
         else:
             for r in range(1, s):
                 num = gorner(num,2,p)
                 if num - p == -1:
-                    simp = 1
+                    prime = 1
                     break
                 elif num == 1: return 0
-        if not simp: return 0
+        if not prime: return 0
     return 1
 
-def generate_pairs():
-    interval = [2**255,2**256]
-    pq, pq1 = [], []
-    for i in range(2):
-        pq.append(find_simple(interval))
-        pq1.append(find_simple(interval))
-    if (pq[0]*pq[1] > pq1[0]*pq1[1]): return generate_pairs()
-    return [pq, pq1]
-
 def GenerateKeyPair():
-    pq = generate_pairs()
-    n,fi,e,d = [],[],[],[]
-    for i in range(2):
-        n.append(pq[i][0]*pq[i][1])
-        fi.append((pq[i][0]-1)*(pq[i][1]-1))
-        e.append(random.randint(2, fi[i]-1))
-        while (gcd(e[i], fi[i]) != 1):
-            e[i] = random.randint(2, fi[i]-1)
-        d.append((inverted(e[i],fi[i]))%fi[i])
-    open_keys = [(e[0],n[0]),(e[1],n[1])]
-    secret_key = d
-    return open_keys, secret_key
+    p = get_prime()
+    q = get_prime()
+    while p == q: p = get_prime()
+    n = p*q
+    phi = (p-1)*(q-1)
+    e = random.randint(2,phi-1)
+    while(gcd(e,phi) != 1): e = random.randint(2,phi-1)
+    d = inverse(e,phi)%phi
+    return (e,n,d)
 
 def Encrypt():
     return 0
