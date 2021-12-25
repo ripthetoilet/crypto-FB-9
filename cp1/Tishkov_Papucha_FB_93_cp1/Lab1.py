3fe03d351e9153f5cf70e8bbcfc0280ce06ed7b1
Nikita Tishkov <nikitatishkov77@gmail.com>
2021-12-25 05:43:29 +0200

    Add sorting for calc_all_bigramm_freq output

--- cp1/Tishkov_Papucha_FB_93_cp1/Lab1.py
+++ cp1/Tishkov_Papucha_FB_93_cp1/Lab1.py
@@ -106,12 +106,11 @@ def calc_all_bigramm_freq(alphabet, text, is_space_allowed = False, is_intersec_
             divider = text_len / 2
         bigramm_freq_dict.update({bigramm: bigramm_num_dict[bigramm]/divider})
 
-    if log_file_name != None:
-        sorted_bigramm_freq_dict = dict(sorted(bigramm_freq_dict.items(), key=lambda item: item[1], reverse=True))
+    sorted_bigramm_freq_dict = dict(sorted(bigramm_freq_dict.items(), key=lambda item: item[1], reverse=True))
+    if log_file_name is not None:
         out_df = pd.DataFrame.from_dict(sorted_bigramm_freq_dict, orient='index', columns=['Frequency'])
         out_df.to_excel(log_file_name + '.xlsx')
-
-    return bigramm_freq_dict
+    return sorted_bigramm_freq_dict
 
 def calc_bigramm_entropy(alphabet, text, is_space_allowed = False, is_intersec_allowed = False):
     bigramm_freq_dict = calc_all_bigramm_freq(alphabet, text, is_space_allowed, is_intersec_allowed)
