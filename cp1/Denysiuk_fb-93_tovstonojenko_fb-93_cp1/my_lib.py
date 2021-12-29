9252334ca533ea4c78101a02a49135e9bd3adf4c
Артем <oster2279@gmail.com>
2021-12-29 09:52:30 +0200

    Revert "Restoring repository"
    This reverts commit 8a73cff31ca2dfbeb1eacdc6ef12e59c7cb48e69.

--- cp1/Denysiuk_fb-93_tovstonojenko_fb-93_cp1/my_lib.py
+++ cp1/Denysiuk_fb-93_tovstonojenko_fb-93_cp1/my_lib.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
 from math import log
 import re
-import os
 
 
 def filter_text(file_name: str, with_whitespace: bool) -> str:
@@ -51,8 +50,3 @@ def calculate_entropy(n: int, stats: dict) -> float:
     for i in stats:
         entropy += stats[i] / quantity * log(stats[i] / quantity, 2)
     return -entropy / n
-
-
-def delete_all_files_in_directory(directory: str):
-    for f in os.listdir(directory):
-        os.remove(os.path.join(directory, f))
